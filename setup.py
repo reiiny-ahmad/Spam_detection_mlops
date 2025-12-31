818282c7cdc6fc3c30fb9efe538a6596aa0bab3f
reiiny-ahmad <jabryahmed2002@gmail.com>
2025-12-31 09:58:31 +0100

    Update-2 -dev/ml

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-# setup.py (à la racine du projet)
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="spam-detection",
-    version="0.1",
-    packages=find_packages(),  # trouve automatiquement api/, model/, etc.
+    name="spam-detection-mlops",
+    version="0.1.0",
+    packages=find_packages(),  # détecte automatiquement api/, model/, etc.
+    install_requires=[],       # pas besoin ici, car requirements.txt gère déjà
 )
